1c15e6d9000581c51d9e241f5fad94f81170db38
Qiming Fang <qf26@cornell.edu>
2017-10-25 18:41:08 -0700

    feat: log to sentry when done.

--- routes/github.js
+++ routes/github.js
@@ -2,6 +2,12 @@ const express = require('express')
 const multer = require('multer')
 const url = require('url')
 const asana = require('asana')
+const Raven = require('raven')
+
+if (process.env.SENTRY_URL) {
+  Raven.config(process.env.SENTRY_URL)
+    .install()
+}
 
 const client = asana.Client.create().useAccessToken(process.env.ASANA_TOKEN)
 
@@ -10,42 +16,53 @@ module.exports = function () {
   const m = multer({})
 
   router.post('/pr', m.any(), (req, res) => {
-    if (req.body) {
-      switch (req.body.action) {
-        case 'closed':
-          const pr = req.body.pull_request
+    try {
+      if (req.body) {
+        switch (req.body.action) {
+          case 'closed':
+            const pr = req.body.pull_request
 
-          if (pr && pr.body && pr.merged) {
-            const splitted = pr.body.split('\r\n')
-            const asanaLines = splitted
-              .filter(line => line.startsWith('https://app.asana'))
+            if (pr && pr.body && pr.merged) {
+              const splitted = pr.body.split('\r\n')
+              const asanaLines = splitted
+                .filter(line => line.startsWith('https://app.asana'))
 
-            asanaLines.forEach(asanaLine => {
-              const asanaPath = url.parse(asanaLine).path
+              asanaLines.forEach(asanaLine => {
+                const asanaPath = url.parse(asanaLine).path
 
-              // example: [ '', '0', '248400113261909', '463141492036657' ]
-              const asanaSegments = asanaPath.split('/')
+                // example: [ '', '0', '248400113261909', '463141492036657' ]
+                const asanaSegments = asanaPath.split('/')
 
-              if (asanaSegments.length === 4) {
-                const taskId = asanaSegments[3]
+                if (asanaSegments.length === 4) {
+                  const taskId = asanaSegments[3]
 
-                client.stories.createOnTask(taskId, {
-                  text: '(Auto Generated) PR:\n' + pr.html_url
-                }).then(res => {
-                  console.log(res)
-                }).catch(err => {
-                  console.error(err)
-                })
-              }
-            })
-          }
+                  client.stories.createOnTask(taskId, {
+                    text: '(Auto Generated) PR:\n' + pr.html_url
+                  }).then(res => {
+                    console.log(res)
+                  }).catch(err => {
+                    console.error(err)
+                    capture(err)
+                  })
+                }
+              })
+            }
 
-          break
+            break
+        }
       }
+    } catch (anything) {
+      capture(anything)
     }
 
     res.sendStatus(200)
   })
 
   return router
+}
+
+function capture (err) {
+  if (process.env.SENTRY_URL) {
+    Raven.captureException(err)
+  }
 }
